3bda8f1cdb283c7eed8b95a017ddc5d2ff63a862
anarbekovv11 <anarbekovvv11@gamil.com>
2023-02-24 23:44:44 +0600

    dop dz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class NameForm extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={ value: " "}
+
+   this.handleChange = this.handleChange.bind()
+   this.handleSubmit = this.handleSubmit.bind()
+
+  }
+handleChange(event) {
+  this.setState({ value: event.target.value.toUpperCase()});
 }
 
-export default App;
+handleSubmit(event) {
+  alert("submit name: " + this.state.value);
+  event.preventDefault();
+}
+render(){
+  return(
+    <form className="form1" onSubmit={this.handleSubmit}>
+      <label>
+        name:
+        <input
+        type="text"
+        value={this.state.value}
+        onChange={this.handleChange}
+        />
+      </label>  
+      <input type="submit" value="Submit" />
+    </form>  
+  );
+ }
+}
+
+export default NameForm;
