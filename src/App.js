0bc6f197713d72d3eb69d1360dc6abbb6df3fac1
anarbekovv11 <anarbekovvv11@gamil.com>
2023-02-25 11:55:27 +0600

    home work4

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import React from "react";
 
-class NameForm extends React.Component{
-  constructor(props){
-    super(props)
-    this.state={ value: " "}
-
-   this.handleChange = this.handleChange.bind()
-   this.handleSubmit = this.handleSubmit.bind()
-
-  }
-handleChange(event) {
-  this.setState({ value: event.target.value.toUpperCase()});
+class Form extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {value: ""}
+		
+		this.handleChange = this.handleChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+	handleChange(e){
+		this.setState({value: e.target.value})
+	}
+	handleSubmit(e){
+		alert("Email address : " + this.state.value).input.value = ""
+		e.preventDefault();
+	}
+	render(){
+		return (
+			<div className="menu">
+        <div className="main-block">
+        <h1>Авторизация</h1>
+				<form onSubmit={this.handleSubmit}>
+					<div className="inputBox">
+						<input type="text" required="required" className="input" value={this.state.value} onChange={this.handleChange}></input>
+						<span class="span">Email</span>
+					</div>
+					<div className="inputBox">
+						<input type="password" required="required" className="input"/>
+						<span class ="span">Password</span>
+					</div>
+					<div className="btn">
+						<input type="submit" value="Регистрация" className="submit"/>
+					</div>
+				</form>
+        </div>
+			</div>	
+		)
+	}
 }
 
-handleSubmit(event) {
-  alert("submit name: " + this.state.value);
-  event.preventDefault();
-}
-render(){
-  return(
-    <form className="form1" onSubmit={this.handleSubmit}>
-      <label>
-        name:
-        <input
-        type="text"
-        value={this.state.value}
-        onChange={this.handleChange}
-        />
-      </label>  
-      <input type="submit" value="Submit" />
-    </form>  
-  );
- }
-}
 
-export default NameForm;
+export default Form;
